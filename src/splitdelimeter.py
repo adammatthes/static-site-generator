2f6c5642c65e83d5eacbb9344fc9195c12b4576e
adammatthes <adammatthes@gmail.com>
2025-07-13 16:00:53 -0400

    Add logic for image and link extraction

--- src/splitdelimeter.py
+++ src/splitdelimeter.py
@@ -1,6 +1,43 @@
 from textnode import TextNode, TextType
 import re
 
+def split_image_node(text):
+    new_nodes = []
+
+    segments = re.findall(r'\!\[.+\]\(.+\)|[^\!\[\]\(\)]+', text)
+
+    for segment in segments:
+        if segment.startswith('!'):
+            alt_text = segment[2:segment.find(']')]
+            url = segment[segment.find('(') + 1: segment.find(')')]
+            new_node = TextNode(alt_text, TextType.IMAGE, url)
+            new_nodes.append(new_node)
+        else:
+            new_node = TextNode(segment, TextType.PLAIN, '')
+            new_nodes.append(new_node)
+
+    return new_nodes
+
+
+def split_link_node(text):
+    new_nodes = []
+
+    segments = re.findall(r'\[.+\]\(.+\)|[^\[\]\(\)]+', text)
+
+    for segment in segments:
+        if segment.startswith('['):
+            link_text = segment[1:segment.find(']')]
+            url = segment[segment.find('(') + 1: segment.find(')')]
+            new_node = TextNode(link_text, TextType.LINK, url)
+            new_nodes.append(new_node)
+        else:
+            new_node = TextNode(segment, TextType.PLAIN, '')
+            new_nodes.append(new_node)
+
+    return new_nodes
+
+
+
 def split_nodes_delimeter(old_nodes, delimeter, text_type):
     new_nodes = []
     delimeters = {TextType.BOLD: "**",
@@ -8,6 +45,16 @@ def split_nodes_delimeter(old_nodes, delimeter, text_type):
                   TextType.CODE: "`",
                   TextType.LINK: ("[", "]", "(", ")"),
                   TextType.IMAGE: ("!", "[", "]", "(", ")")}
+
+    if text_type == TextType.IMAGE:
+        for node in old_nodes:
+            new_nodes.extend(split_image_node(node.text))
+        return new_nodes
+
+    if text_type == TextType.LINK:
+        for node in old_nodes:
+            new_nodes.extend(split_link_node(node.text))
+        return new_nodes
 
 
     for node in old_nodes:
--- src/test_splitdelimeter.py
+++ src/test_splitdelimeter.py
@@ -12,8 +12,18 @@ class TestSplitDelimeter(unittest.TestCase):
     def test_two_embed(self):
         node = TextNode("This is _text_ with a **bold** word", TextType.PLAIN, '')
         temp_nodes = split_nodes_delimeter([node],  "**", TextType.BOLD)
-        print(temp_nodes)
         new_nodes = split_nodes_delimeter(temp_nodes, "_", TextType.ITALIC)
-        print(new_nodes)
         self.assertEqual(len(new_nodes), 5)
         self.assertEqual([n.text_type for n in new_nodes], [TextType.PLAIN, TextType.ITALIC, TextType.PLAIN, TextType.BOLD, TextType.PLAIN])
+
+    def test_image_extract(self):
+        node = TextNode("Check out ![my dog](www.cuteanimals.com)", TextType.PLAIN, '')
+        new_nodes = split_nodes_delimeter([node], '!', TextType.IMAGE)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual([n.text_type for n in new_nodes], [TextType.PLAIN, TextType.IMAGE])
+
+    def test_link_extract(self):
+        node = TextNode("Follow my socials [here](www.link.tr)", TextType.PLAIN, '')
+        new_nodes = split_nodes_delimeter([node], '[', TextType.LINK)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual([n.text_type for n in new_nodes], [TextType.PLAIN, TextType.LINK])
